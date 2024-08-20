054b27d49a60023012768e0b6be1149fd83d69d8
Navneet Prakash <navprak@gmail.com>
2024-08-20 15:15:43 +1200

    added navigation links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,13 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Placeorder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
+// Routes for the application
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:[px-7vw] lg:px[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
