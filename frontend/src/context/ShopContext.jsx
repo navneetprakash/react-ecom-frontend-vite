9331c64cda3f39aed48450a13df3e940bb3bdb2d
Navneet Prakash <navprak@gmail.com>
2024-08-22 11:42:31 +1200

    Add to cart counter added when button is clicked, applied react toast notification when no items are selected when adding to cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 import PropTypes from 'prop-types';
+import { toast } from "react-toastify";
 
 // export this context to be used in other components
 export const ShopContext = createContext();
@@ -11,10 +12,55 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
 
+  const addToCart = async (itemId, size) => {
+
+    if (!size) {
+      toast.error("Please select size");
+      return;
+    }
+
+    // clone cart items
+    let cartData = structuredClone(cartItems);
+
+    if (!cartData[itemId]) {
+      cartData[itemId] = { [size]: 1 };
+    } else {
+      if (!cartData[itemId][size]) {
+        cartData[itemId][size] = 1;
+      } else {
+        cartData[itemId][size]++;
+      }
+    }
+    setCartItems({ ...cartData });
+
+  };
+
+  // get cart count
+  const getCartCount = () => {
+    let totalCount = 0;
+
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          // EDIT LATER
+          toast.error(error);
+        }
+      }
+    }
+    return totalCount;
+  };
+
+  // context value to be passed to other components
   const value = {
     products, currency, delivery_fee,
-    search, setSearch, showSearch, setShowSearch
+    search, setSearch, showSearch, setShowSearch,
+    cartItems, addToCart, getCartCount
   };
 
   return (
